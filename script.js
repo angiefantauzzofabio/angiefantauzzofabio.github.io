2f1f247604d2fac31a626590981f9919c4956bdd
angiefantauzzofabio <angiefafa21@gmail.com>
2022-09-09 18:00:17 -0300

    mejoras

--- script.js
+++ script.js
@@ -151,3 +151,29 @@ window.onclick = function(event) {
     modal5.style.display = "none";
   }
 }
+
+// Get the modal
+var modal6 = document.getElementById("colaborar");
+
+// Get the button that opens the modal
+var btn6 = document.getElementById("botonColaborar");
+
+// Get the <span> element that closes the modal
+var span6 = document.getElementsByClassName("close6")[0];
+
+// When the user clicks on the button, open the modal
+btn6.onclick = function() {
+  modal6.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span6.onclick = function() {
+	modal6.style.display = "none";
+  }
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal6) {
+    modal6.style.display = "none";
+  }
+}
