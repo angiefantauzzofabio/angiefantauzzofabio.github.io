357358dfd9a95fdd63f710712cbfa7ac54cbcd9d
angiefantauzzofabio <angiefafa21@gmail.com>
2023-01-09 21:10:03 -0300

    changes

--- script.js
+++ script.js
@@ -49,28 +49,44 @@ window.onclick = function(event) {
 }
 
 // Get the modal
-var modal2 = document.getElementById("noticia");
+var modal2 = document.getElementById("noticia1");
+var modal2_not = document.getElementById("noticia2");
+
 
 // Get the button that opens the modal
-var btn2 = document.getElementById("botonNoticia");
+var btn2 = document.getElementById("botonNoticia1");
+var btn2_not = document.getElementById("botonNoticia2");
+
 
 // Get the <span> element that closes the modal
-var span2 = document.getElementsByClassName("close2")[0];
+var span2 = document.getElementsByClassName("close2_noticia1")[0];
+var span2_not = document.getElementsByClassName("close2_noticia2")[0];
+
 
 // When the user clicks on the button, open the modal
 btn2.onclick = function() {
   modal2.style.display = "block";
+}
+btn2_not.onclick = function() {
+  modal2_not.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span2.onclick = function() {
 	modal2.style.display = "none";
   }
-
+span2_not.onclick = function() {
+    modal2_not.style.display = "none";
+  }
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal2) {
     modal2.style.display = "none";
+  }
+}
+window.onclick = function(event) {
+  if (event.target == modal2_not) {
+    modal2_not.style.display = "none";
   }
 }
 
